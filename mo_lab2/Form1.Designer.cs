74cfbe500438dadf30ad8b669a54b3d04556618b
%40e.kuzmin.2013 <e.kuzmin.2013%40stud.nstu.ru>
2019-03-12 21:34:44 +0700

    rename variables

--- mo_lab2/Form1.Designer.cs
+++ mo_lab2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace mo_lab2
 {
-    partial class Form1
+    partial class OneDimensionalSearchForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -48,28 +48,28 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.textBox8 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnShowChart = new System.Windows.Forms.Button();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.tbOutDichotomy = new System.Windows.Forms.TextBox();
+            this.tbOutGolden = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.Name = "График";
-            this.chart1.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "График";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(560, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -210,25 +210,25 @@
             this.textBox8.TabIndex = 15;
             this.textBox8.Text = "0,001";
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(11, 596);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Провести расчеты";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnStart.Location = new System.Drawing.Point(11, 596);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(114, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Провести расчеты";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // button2
+            // btnShowChart
             // 
-            this.button2.Location = new System.Drawing.Point(131, 596);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 23);
-            this.button2.TabIndex = 20;
-            this.button2.Text = "Показать график";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnShowChart.Location = new System.Drawing.Point(131, 596);
+            this.btnShowChart.Name = "btnShowChart";
+            this.btnShowChart.Size = new System.Drawing.Size(114, 23);
+            this.btnShowChart.TabIndex = 20;
+            this.btnShowChart.Text = "Показать график";
+            this.btnShowChart.UseVisualStyleBackColor = true;
+            this.btnShowChart.Click += new System.EventHandler(this.btnShowChart_Click);
             // 
             // label9
             // 
@@ -239,23 +239,23 @@
             this.label9.TabIndex = 21;
             this.label9.Text = "Метод дихотомии";
             // 
-            // textBox9
+            // tbOutDichotomy
             // 
-            this.textBox9.Location = new System.Drawing.Point(12, 490);
-            this.textBox9.Multiline = true;
-            this.textBox9.Name = "textBox9";
-            this.textBox9.ReadOnly = true;
-            this.textBox9.Size = new System.Drawing.Size(219, 100);
-            this.textBox9.TabIndex = 22;
+            this.tbOutDichotomy.Location = new System.Drawing.Point(12, 490);
+            this.tbOutDichotomy.Multiline = true;
+            this.tbOutDichotomy.Name = "tbOutDichotomy";
+            this.tbOutDichotomy.ReadOnly = true;
+            this.tbOutDichotomy.Size = new System.Drawing.Size(219, 100);
+            this.tbOutDichotomy.TabIndex = 22;
             // 
-            // textBox10
+            // tbOutGolden
             // 
-            this.textBox10.Location = new System.Drawing.Point(253, 490);
-            this.textBox10.Multiline = true;
-            this.textBox10.Name = "textBox10";
-            this.textBox10.ReadOnly = true;
-            this.textBox10.Size = new System.Drawing.Size(219, 100);
-            this.textBox10.TabIndex = 24;
+            this.tbOutGolden.Location = new System.Drawing.Point(253, 490);
+            this.tbOutGolden.Multiline = true;
+            this.tbOutGolden.Name = "tbOutGolden";
+            this.tbOutGolden.ReadOnly = true;
+            this.tbOutGolden.Size = new System.Drawing.Size(219, 100);
+            this.tbOutGolden.TabIndex = 24;
             // 
             // label10
             // 
@@ -266,17 +266,17 @@
             this.label10.TabIndex = 23;
             this.label10.Text = "Метод золотого сечения";
             // 
-            // Form1
+            // OneDimensionalSearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 631);
-            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.tbOutGolden);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.tbOutDichotomy);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnShowChart);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label8);
@@ -294,7 +294,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.chart1);
-            this.Name = "Form1";
+            this.Name = "OneDimensionalSearchForm";
             this.Text = "Методы дихотомии и золотого сечения";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
@@ -321,11 +321,11 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnShowChart;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox tbOutDichotomy;
+        private System.Windows.Forms.TextBox tbOutGolden;
         private System.Windows.Forms.Label label10;
     }
 }
--- mo_lab2/Form1.cs
+++ mo_lab2/Form1.cs
@@ -10,19 +10,19 @@ using System.Windows.Forms;
 
 namespace mo_lab2
 {
-    public partial class Form1 : Form
+    public partial class OneDimensionalSearchForm : Form
     {
         double x3, x2, x, c, a0, b0, e, d, s;
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e)
         {
             dichotomy();
-            golden_ratio2();
+            golden_ratio();
 
             draw();
         }
 
-        public Form1()
+        public OneDimensionalSearchForm()
         {
             InitializeComponent();
         }
@@ -98,145 +98,17 @@ namespace mo_lab2
                 }
             }
 
-            textBox9.Text = "";
-            textBox9.AppendText("Количество итераций: " + k + "\n");
-            textBox9.AppendText("Количество вычислений: " + r + "\n\n");
-            textBox9.AppendText("Конечные значения:\n");
-            textBox9.AppendText("an = " + Convert.ToString(a1) + "\n");
-            textBox9.AppendText("bn = " + Convert.ToString(b1) + "\n");
-            textBox9.AppendText("x = " + Convert.ToString((a1 + b1) / 2) + "\n");
-            textBox9.AppendText("y = " + Convert.ToString(func(x3, x2, x, c, ((a1 + b1) / 2))) + "\n");
+            tbOutDichotomy.Text = "";
+            tbOutDichotomy.AppendText("Количество итераций: " + k + "\n");
+            tbOutDichotomy.AppendText("Количество вычислений: " + r + "\n\n");
+            tbOutDichotomy.AppendText("Конечные значения:\n");
+            tbOutDichotomy.AppendText("an = " + Convert.ToString(a1) + "\n");
+            tbOutDichotomy.AppendText("bn = " + Convert.ToString(b1) + "\n");
+            tbOutDichotomy.AppendText("x = " + Convert.ToString((a1 + b1) / 2) + "\n");
+            tbOutDichotomy.AppendText("y = " + Convert.ToString(func(x3, x2, x, c, ((a1 + b1) / 2))) + "\n");
         }
 
         void golden_ratio()
-        {
-            bool flag = false; //false = вычисляется точка x1, иначе вычисляется точка x2
-            int k = 0; //Количество итераций
-            int r = 0; //Количество вычислений
-
-            double a1, b1;
-            double f1 = 0, f2 = 0;
-            double x_1 = 0, x_2 = 0;
-
-            x3 = Convert.ToDouble(textBox1.Text);
-            x2 = Convert.ToDouble(textBox2.Text);
-            x = Convert.ToDouble(textBox3.Text);
-            c = Convert.ToDouble(textBox4.Text);
-
-            a0 = Convert.ToDouble(textBox5.Text);
-            b0 = Convert.ToDouble(textBox6.Text);
-            e = Convert.ToDouble(textBox7.Text);
-            d = Convert.ToDouble(textBox8.Text);
-
-            a1 = a0; b1 = b0;
-
-            while (k < 1000)
-            {
-                k += 1;
-
-                if (Math.Abs(b1 - a1) < e) //Если решение было найдено, прерываем цикл
-                {
-                    break;
-                }
-
-                if(k == 1) //Если итерация первая, вычисляем оба значения
-                {
-                    x_1 = a1 + (0.381966011 * (b1 - a1));
-                    x_2 = a1 + (0.6180033989 * (b1 - a1));
-
-                    f1 = func(x3, x2, x, c, x_1);
-                    f2 = func(x3, x2, x, c, x_2);
-
-
-                    if(f1 < f2)
-                    {
-                        a1 = a0;
-                        b1 = x_2;
-                        x_2 = x_1;
-                        flag = false;
-                    }
-                    else
-                    {
-                        a1 = x_1;
-                        b1 = b0;
-                        x_1 = x_2;
-                        flag = true;
-                    }
-
-                    r += 4;
-                }
-                else //Если итерация не 1-ая
-                {
-                    if(flag == false) //Вычисляем x1 и f1
-                    {
-                        x_1 = a1 + (0.381966011 * (b1 - a1));
-                        f1 = func(x3, x2, x, c, x_1);
-                    }
-                    else //Вычисляем x2 и f2
-                    {
-                        x_2 = a1 + (0.6180033989 * (b1 - a1));
-                        f2 = func(x3, x2, x, c, x_2);
-                    }
-
-                    if (f1 < f2)
-                    {
-                        b1 = x_2;
-                        x_2 = x_1;
-                        flag = false;
-                    }
-                    else
-                    {
-                        a1 = x_1;
-                        x_1 = x_2;
-                        flag = true;
-                    }
-
-                    r += 2;
-                }
-            }
-
-            textBox10.Text = "";
-            textBox10.AppendText("Количество итераций: " + k + "\n");
-            textBox10.AppendText("Количество вычислений: " + r + "\n");
-            textBox10.AppendText("Конечные значения:\n");
-            textBox10.AppendText("an = " + Convert.ToString(a1) + "\n");
-            textBox10.AppendText("bn = " + Convert.ToString(b1) + "\n");
-        }
-
-        void minimize(double f, double e, double a, double b)
-        {
-            //double f = (1 + Math.Sqrt(5)) / 2;
-            double x_1, x_2, y_1, y_2;
-            if (Math.Abs(b - a) < e) //Если решение было найдено, прерываем цикл
-            {
-                textBox10.Text = "";
-                //textBox10.AppendText("Количество итераций: " + k + "\n");
-                //textBox10.AppendText("Количество вычислений: " + r + "\n");
-                textBox10.AppendText("Конечные значения:\n");
-                textBox10.AppendText("an = " + Convert.ToString(a) + "\n");
-                textBox10.AppendText("bn = " + Convert.ToString(b) + "\n");
-            }
-            else
-            {
-                x_1 = b - ((b - a) / f);
-                x_2 = a + ((b - a) / f);
-
-                y_1 = func(x3, x2, x, c, x_1);
-                y_2 = func(x3, x2, x, c, x_2);
-
-                if (y_1 > y_2)
-                {
-                    a = x_1;
-                }
-                else
-                {
-                    b = x_2;
-                }
-            }
-
-        }
-
-        void golden_ratio2()
         {
             int k = 0; //Количество итераций
             int r = 0; //Количество вычислений
@@ -285,17 +157,17 @@ namespace mo_lab2
                 }
             }
 
-            textBox10.Text = "";
-            textBox10.AppendText("Количество итераций: " + k + "\n");
-            textBox10.AppendText("Количество вычислений: " + r + "\n");
-            textBox10.AppendText("Конечные значения:\n");
-            textBox10.AppendText("an = " + Convert.ToString(a) + "\n");
-            textBox10.AppendText("bn = " + Convert.ToString(b) + "\n");
-            textBox10.AppendText("x = " + Convert.ToString((a + b) / 2) + "\n");
-            textBox10.AppendText("y = " + Convert.ToString(func(x3, x2, x, c, ((a + b) / 2))) + "\n");
+            tbOutGolden.Text = "";
+            tbOutGolden.AppendText("Количество итераций: " + k + "\n");
+            tbOutGolden.AppendText("Количество вычислений: " + r + "\n");
+            tbOutGolden.AppendText("Конечные значения:\n");
+            tbOutGolden.AppendText("an = " + Convert.ToString(a) + "\n");
+            tbOutGolden.AppendText("bn = " + Convert.ToString(b) + "\n");
+            tbOutGolden.AppendText("x = " + Convert.ToString((a + b) / 2) + "\n");
+            tbOutGolden.AppendText("y = " + Convert.ToString(func(x3, x2, x, c, ((a + b) / 2))) + "\n");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnShowChart_Click(object sender, EventArgs e)
         {
             draw();
         }
